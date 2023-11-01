23ed9fd73d6fbc548a5eaa0d7490892edcc208cf
Jack Murray <jmurray2399@gmail.comm>
2023-11-01 15:02:16 +1000

    Game Border Applied

--- sketch.js
+++ sketch.js
@@ -47,97 +47,97 @@ function preload() {
 
 function setup() {
   createCanvas(1000, 600);
-  goldSpawnTimerExec = floor(random(0, 900));           //Initialises First Rand Num For Resource Spawning -- Use of floor() to round to nearest whole int                      
-  wormSpawnTimerExec = floor(random(0, 600));           //Initialises First Rand Num For Resource Spawning -- Use of floor() to round to nearest whole int                      
+  goldSpawnTimerExec = floor(random(0, 900));             //Initialises First Rand Num For Resource Spawning -- Use of floor() to round to nearest whole int                      
+  wormSpawnTimerExec = floor(random(0, 600));             //Initialises First Rand Num For Resource Spawning -- Use of floor() to round to nearest whole int                      
 
 
-  GoldCoinArray.push(GoldCoin1,                         //Push Preloaded Images Into Array
-    GoldCoin2,                                          //Push Preloaded Images Into Array
-    GoldCoin3,                                          //Push Preloaded Images Into Array
-    GoldCoin4,                                          //Push Preloaded Images Into Array
-    GoldCoin5,                                          //Push Preloaded Images Into Array
-    GoldCoin6,                                          //Push Preloaded Images Into Array
-    GoldCoin7,                                          //Push Preloaded Images Into Array
-    GoldCoin8,                                          //Push Preloaded Images Into Array
-    GoldCoin9);                                         //Push Preloaded Images Into Array
+  GoldCoinArray.push(GoldCoin1,                           //Push Preloaded Images Into Array
+    GoldCoin2,                                            //Push Preloaded Images Into Array
+    GoldCoin3,                                            //Push Preloaded Images Into Array
+    GoldCoin4,                                            //Push Preloaded Images Into Array
+    GoldCoin5,                                            //Push Preloaded Images Into Array
+    GoldCoin6,                                            //Push Preloaded Images Into Array
+    GoldCoin7,                                            //Push Preloaded Images Into Array
+    GoldCoin8,                                            //Push Preloaded Images Into Array
+    GoldCoin9);                                           //Push Preloaded Images Into Array
 
-  player = new Sprite(100, 100);                        //Correlates to First sprite and keyboard movement
-  player.collider = 'kinematic'                         //Removes rotation of spite after collision
+  player = new Sprite(200, 200);                          //Correlates to First sprite and keyboard movement
+  player.collider = 'kinematic'                           //Removes rotation of spite after collision
 
 }
 
 function draw() {
-  goldSpawnTimer--;                                     //Decrements Timer
-  wormSpawnTimer--;                                     //Decrements Timer
+  goldSpawnTimer--;                                       //Decrements Timer
+  wormSpawnTimer--;                                       //Decrements Timer
 
   background('black');
-  resourceStatusBar();                                  //Runs Custom Function
-  resourceCollectionMechanics();                        //Runs Custom Function
-  p1Movement();                                         //Runs Custom Keyboard Movement Function
+  resourceStatusBar();                                    //Runs Custom Function
+  resourceCollectionMechanics();                          //Runs Custom Function
+  p1Movement();                                           //Runs Custom Keyboard Movement Function
 
-  if (goldSpawnTimerExec == goldSpawnTimer) {           //Allows For random spawn time within 30 secconds
-    goldOreArray.push(goldGeneration());                //Runs Custom Function and stores in array to check for collisions
-    goldSpawnTimer = 1800;
-    goldSpawnTimerExec = floor(random(0, 1800));        //Use of floor() to round to nearest whole int
-  }
-  if (wormSpawnTimerExec == wormSpawnTimer) {           //Allows For random spawn time within 10 secconds
-    wormArray.push(wormGeneration());                   //Runs Custom Function and stores in array to check for collisions
-    wormSpawnTimer = 600;
-    wormSpawnTimerExec = floor(random(0, 600));         //Use of floor() to round to nearest whole int
-  }
 
-for(let i=0; i<goldOreArray.length; i++) {                  //Loops through GoldOreArray
-    if(goldOreArray[i].collides(player)){                   //Checks for collision between each instance of array and play
-      fill('green');
-      rect(goldOreArray[i], goldOreArray[i]-30, 40, 15);
-      goldOreArray[i].remove();                             //Removes Sprite after collision
-      goldCount++;                                          
+
+  function resourceStatusBar() {                          //Draws The Status Bar in the top left corner
+    timer--;                                              //Used to animate coin
+
+    stroke('white')                                       //Outline|Border of box
+    noFill()                                              //Outline|Border of box
+    rect(10, 10, 200, 40, 5);                             //Outline|Border of box
+
+    if (timer == 0) {
+      GoldCoinIndex++;                                    //Increments index
+      if (GoldCoinIndex >= GoldCoinArray.length) {        //Resets Index allowing for animation
+        GoldCoinIndex = 0;
+      }
+      timer = 15;                                         //Resets Timer Used to stagger coin animation
     }
-}
 
-for(let i=0; i<wormArray.length; i++) {                     //Loops through WormArray
-  if(wormArray[i].collides(player)){                        //Checks for collision between each instance of array and player
-    fill('green');
-    rect(wormArray[i], wormArray[i]-30, 40, 15);
-    wormArray[i].remove();                                  //Removes Sprite after collision
-    wormCount++;                                            //Adds to the currency in top left
+    image(GoldCoinArray[GoldCoinIndex], 80, 15);          //Draws the coin stored at a specific index of the array
+    image(wormImg, 15, 15)                                //Draws Worm in status bar
+
+    textAlign(LEFT, CENTER);                              //Displays GoldCount Value in Status Bar
+    textSize(18);                                         //Displays GoldCount Value in Status Bar 
+    noStroke();                                           //Displays GoldCount Value in Status Bar
+    fill('gold');                                         //Displays GoldCount Value in Status Bar
+    text(goldCount, 110, 33);                             //Displays GoldCount Value in Status Bar
+    fill('#d36d5d')                                       //Displays WormCount
+    text(wormCount, 45, 33)                               //Displays WormCount
   }
-}
-}
 
-function resourceStatusBar() {                          //Draws The Status Bar in the top left corner
-  timer--;                                              //Used to animate coin
-
-  stroke('white')                                       //Outline|Border of box
-  noFill()                                              //Outline|Border of box
-  rect(10, 10, 200, 40, 5);                             //Outline|Border of box
-
-  if (timer == 0) {
-    GoldCoinIndex++;                                    //Increments index
-    if (GoldCoinIndex >= GoldCoinArray.length) {        //Resets Index allowing for animation
-      GoldCoinIndex = 0;
+  function resourceCollectionMechanics() {
+    if (goldSpawnTimerExec == goldSpawnTimer) {           //Allows For random spawn time within 30 secconds
+      goldOreArray.push(goldGeneration());                //Runs Custom Function and stores in array to check for collisions
+      goldSpawnTimer = 1800;
+      goldSpawnTimerExec = floor(random(0, 1800));        //Use of floor() to round to nearest whole int
     }
-    timer = 15;                                         //Resets Timer Used to stagger coin animation
+    if (wormSpawnTimerExec == wormSpawnTimer) {           //Allows For random spawn time within 10 secconds
+      wormArray.push(wormGeneration());                   //Runs Custom Function and stores in array to check for collisions
+      wormSpawnTimer = 600;
+      wormSpawnTimerExec = floor(random(0, 600));         //Use of floor() to round to nearest whole int
+    }
+
+    for (let i = 0; i < goldOreArray.length; i++) {               //Loops through GoldOreArray
+      if (goldOreArray[i].collides(player)) {                     //Checks for collision between each instance of array and play
+        fill('green');
+        rect(goldOreArray[i], goldOreArray[i] - 30, 40, 15);
+        goldOreArray[i].remove();                                 //Removes Sprite after collision
+        goldCount++;
+      }
+    }
+
+    for (let i = 0; i < wormArray.length; i++) {                  //Loops through WormArray
+      if (wormArray[i].collides(player)) {                        //Checks for collision between each instance of array and player
+        fill('green');
+        rect(wormArray[i], wormArray[i] - 30, 40, 15);
+        wormArray[i].remove();                                    //Removes Sprite after collision
+        wormCount++;                                              //Adds to the currency in top left
+      }
+    }
   }
-
-  image(GoldCoinArray[GoldCoinIndex], 10, 15);          //Draws the coin stored at a specific index of the array
-  image(wormImg, 120, 15)                               //Draws Worm in status bar
-
-  textAlign(LEFT, CENTER);                              //Displays GoldCount Value in Status Bar
-  textSize(18);                                         //Displays GoldCount Value in Status Bar 
-  noStroke();                                           //Displays GoldCount Value in Status Bar
-  fill('gold');                                         //Displays GoldCount Value in Status Bar
-  text(goldCount, 40, 33);                              //Displays GoldCount Value in Status Bar
-  fill('#d36d5d')                                       //Displays WormCount
-  text(wormCount, 150, 33)                              //Displays WormCount
-}
-
-function resourceCollectionMechanics() {
-
 }
 
 function goldGeneration() {
-  let goldOre = new Sprite()                            //Creates Gold Ore Sprite
+  let goldOre = new Sprite()                                      //Creates Gold Ore Sprite
   goldOre.img = goldOreImg
   goldOre.scale = 0.1
   goldOre.x = random(60, width - 60)
@@ -148,28 +148,28 @@ function goldGeneration() {
 }
 
 function wormGeneration() {
-  let worm = new Sprite()                              //Creates Worm Sprite
+  let worm = new Sprite()                                         //Creates Worm Sprite
   worm.img = wormImg
   worm.scale = 2
   worm.x = random(60, width - 60)
   worm.y = random(60, height - 60)
   worm.w = 45
   worm.h = 15
-  
+
   worm.debug = true;
   return worm
 }
 
 function p1Movement() {
-  if (kb.pressing('arrowUp')) {                   //Vertical Movement
+  if (kb.pressing('arrowUp') && player.y > 60 + player.h / 2) {                           //Vertical Movement
     player.vel.y = -10;
-  } else if (kb.pressing('arrowDown')) {
+  } else if (kb.pressing('arrowDown') && player.y < height - 60 - player.h / 2) {
     player.vel.y = +10
   } else player.vel.y = 0
 
-  if (kb.pressing('arrowRight')) {                //Horizontal Movement
+  if (kb.pressing('arrowRight') && player.x < width - 60 - player.w / 2) {                //Horizontal Movement
     player.vel.x = +10;
-  } else if (kb.pressing('arrowLeft')) {
+  } else if (kb.pressing('arrowLeft') && player.x > 60 + player.w / 2) {
     player.vel.x = -10
   } else player.vel.x = 0
 }
